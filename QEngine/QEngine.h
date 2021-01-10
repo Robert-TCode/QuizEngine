3d614520fded51e4c025b21d97174472e2f80982
Robert Tanase <robert.tanase97@gmail.com>
2021-01-10 13:26:52 +0000

    Initial Commit

---
+++ QEngine/QEngine.h
@@ -0,0 +1,18 @@
+//
+//  QEngine.h
+//  QEngine
+//
+//  Created by TCode on 10/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QEngine.
+FOUNDATION_EXPORT double QEngineVersionNumber;
+
+//! Project version string for QEngine.
+FOUNDATION_EXPORT const unsigned char QEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QEngine/PublicHeader.h>
+
+
