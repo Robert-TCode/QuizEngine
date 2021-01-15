ec8695307ca4df4968ea18becede76cfc78700bf
Robert Tanase <robert.tanase97@gmail.com>
2021-01-15 00:50:08 +0000

    Remove hacky method

--- QEngine/Game.swift
+++ QEngine/Game.swift
@@ -22,27 +22,13 @@ public func startGame<Question, Answer: Equatable, R: Router>(
 
 private func scoring<Question: Hashable, Answer: Equatable>(_ answers: [Question: Answer], correctAnswers: [Question: Answer]) -> Int  {
     return answers.reduce(0) { (score, tuple) in
-        var newPoints = 0
 
         // Here is a VERY good example why using protocols where they are not necessary over concrete types might lead to other complications.
-        // Arrays are Equatable since Swift 4.2 BUT the order does matter.
-        // Therefore I could use a Set instead of an Array for Answer type.
+        // Arrays are Equatable since Swift 4.2 BUT the order does matter, which we don't want or multiple answer questions.
+        // Therefore I have to use a Set instead of an Array for Answer type.
         // Trying to cast something to an array of Generics DOEN'T work, otherwise I would've had [T: Decodable] below.
 
-        if let value = tuple.value as? [String],
-           let correctValue = correctAnswers[tuple.key] as? [String] {
-            newPoints = correctValue.containsSameElements(as: value) ? 1 : 0
-        } else {
-            newPoints = correctAnswers[tuple.key] == tuple.value ? 1 : 0
-        }
+        let  newPoints = correctAnswers[tuple.key] == tuple.value ? 1 : 0
         return score + newPoints
-    }
-}
-
-// Hacky way of solving the Array order at comparation issue.
-// [String] doesn't conform to Comaprable (duuh) so I can't use Answer: Comparable :(
-public extension Array where Element: Comparable {
-    func containsSameElements(as other: [Element]) -> Bool {
-        return self.count == other.count && self.sorted() == other.sorted()
     }
 }
